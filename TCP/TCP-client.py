82aa5516311f164e3ab0a4f4ca2f0653e80d6563
Thomasov <thomasoverfield@rocketmail.com>
2016-09-22 09:56:26 -0500

    Changed port number in tcp-server file and added TML to python file

--- TCP/TCP-client.py
+++ TCP/TCP-client.py
@@ -25,6 +25,7 @@ def validateInput(input):
 TCP_IP = '127.0.0.1'
 TCP_PORT = 10022
 BUFFER_SIZE = 1024
+TML = 8
 
 breakFlag = 1;
 while (breakFlag):
@@ -35,7 +36,7 @@ while (breakFlag):
 		if validateInput(MESSAGE):
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.connect((TCP_IP, TCP_PORT))
-			s.send(str(MESSAGE))
+			s.send()
 			data = s.recv(BUFFER_SIZE)
 			s.close()
 			print "received data:", data
